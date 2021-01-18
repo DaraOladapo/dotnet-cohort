05edbb345c5b989e1e6142cee1eade244b6722eb
Dara Oladapo <doladapo@qa.com>
2021-01-18 14:58:29 +0000

    Get Cars

---
+++ code/Web/CarsWebMVC/Controllers/CarsController.cs
@@ -0,0 +1,25 @@
+﻿using CarsWebLibrary;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarsWebMVC.Controllers
+{
+    public class CarsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var cars = CarService.GetCars();
+            return View(cars);
+        }
+        [Route("details/{guid}")]
+        public IActionResult Details(Guid guid)
+        {
+            var car = CarService.GetCar(guid);
+            return View(car);
+        }
+    }
+}
