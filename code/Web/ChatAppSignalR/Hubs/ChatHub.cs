1ca9841a2f37d0e51de05310b1c1a5c8c029bb29
Dara Oladapo <doladapo@qa.com>
2021-01-20 12:36:58 +0000

    Chatty

---
+++ code/Web/ChatAppSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatAppSignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async override Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "Chat Users");
+            await base.OnConnectedAsync();
+        }
+        public async override Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "Chat Users");
+            await base.OnDisconnectedAsync(exception);
+        }
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
