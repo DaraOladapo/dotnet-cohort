a1a8e2d7973b895b0c9bf7344750780f569190d8
Dara Oladapo <doladapo@qa.com>
2021-01-28 09:38:11 +0000

    Signal Client Connection

---
+++ code/Web/ChatAppClient/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ChatAppClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HubConnection connection;
+        public MainWindow()
+        {
+            InitializeComponent();
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:44315/chathub")
+                .WithAutomaticReconnect(new RandomRetryPolicy())
+                .Build();
+            connection.Reconnecting += error =>
+            {
+                Debug.Assert(connection.State == HubConnectionState.Reconnecting);
+                return Task.CompletedTask;
+            };
+            connection.Reconnected += connectionId =>
+            {
+                Debug.Assert(connection.State == HubConnectionState.Connected);
+                return Task.CompletedTask;
+            };
+            connection.Closed += error =>
+            {
+                Debug.Assert(connection.State == HubConnectionState.Disconnected);
+                return Task.CompletedTask;
+            };
+        }
+        public static async Task<bool> ConnectWithRetryAsync(HubConnection connection, CancellationToken token)
+        {
+            while (true)
+            {
+                try
+                {
+                    await connection.StartAsync(token);
+                    Debug.Assert(connection.State == HubConnectionState.Connected);
+                    return true;
+                }
+                catch when (token.IsCancellationRequested)
+                {
+                    return false;
+                }
+                catch
+                {
+                    // Failed to connect, trying again in 5000 ms.
+                    Debug.Assert(connection.State == HubConnectionState.Disconnected);
+                    await Task.Delay(5000);
+                }
+            }
+
+        }
+        private async void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await connection.StartAsync();
+                messagesList.Items.Add("Connection started");
+                connectButton.IsEnabled = false;
+                sendButton.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                messagesList.Items.Add(ex.Message);
+            }
+
+            connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    var newMessage = $"{user}: {message}";
+                    messagesList.Items.Add(newMessage);
+                });
+            });
+        }
+
+        private async void sendButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await connection.InvokeAsync("SendMessage",
+                    userTextBox.Text, messageTextBox.Text);
+            }
+            catch (Exception ex)
+            {
+                messagesList.Items.Add(ex.Message);
+            }
+        }
+    }
+}
---
+++ code/Web/ChatAppClient/RandomRetryPolicy.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+
+namespace ChatAppClient
+{
+    public class RandomRetryPolicy : IRetryPolicy
+    {
+        private readonly Random _random = new Random();
+
+        public TimeSpan? NextRetryDelay(RetryContext retryContext)
+        {
+            if (retryContext.ElapsedTime < TimeSpan.FromSeconds(60))
+            {
+                return TimeSpan.FromSeconds(_random.NextDouble() * 10);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
